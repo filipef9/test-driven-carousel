9c7a7cf0046d23d14d8496ac279562d8e16e48c8
filipef9 <filipef9@hotmail.com>
2022-03-25 13:43:37 +0000

    :wrench: Initial stylelint setup.

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,7 +5,12 @@ import CarouselSlide from './CarouselSlide';
 
 export default class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes)).isRequired,
+  }
+
+  static defaultProps = {
+    defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   }
 
   state = {
@@ -23,11 +28,14 @@ export default class Carousel extends React.PureComponent {
   }
 
   render() {
-    const { slides, ...rest } = this.props;
+    const { defaultImgHeight, slides, ...rest } = this.props;
 
     return (
       <div {...rest}>
-        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselSlide 
+          imgHeight={defaultImgHeight}
+          {...slides[this.state.slideIndex]} 
+        />
         <CarouselButton
           data-action="prev"
           onClick={this.handlePrevClick}
